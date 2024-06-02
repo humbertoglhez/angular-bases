d2f05038f8576e62ee1c623f04408ade14d5c910
humbertoglhez <humbertoglhez@gmail.com>
2024-06-01 20:11:45 -0700

    Fin de sección 4

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css',
+})
+export class HeroComponent {
+  public name: string = 'iron man';
+  public age: number = 45;
+
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDesciption(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero(): void {
+    this.name = 'spiterman';
+  }
+  changeAge(): void {
+    this.age = 25;
+  }
+  resetForm(): void {
+    this.name = 'iron man';
+    this.age = 45;
+  }
+}
